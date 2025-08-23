6d275c35aa2f0a931c29182f711293301aafe3ca
ClemersonFT <165335337+ClemersonFT@users.noreply.github.com>
2025-08-23 14:04:49 -0300

    Delete Trabalho_POO_BDA directory

--- Trabalho_POO_BDA/src/imobiliaria/dao/BaseDAO.java
+++
@@ -1,9 +0,0 @@
-package imobiliaria.dao;
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public class BaseDAO {
-    protected Connection c() throws SQLException {
-        return ConnectionFactory.getInstance().get();
-    }
-}
